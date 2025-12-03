1a52018123df7371f82324fe497e8f08b5e37dbc
Diogoazevedo24891 <a24891@alunos.ipca.pt>
2025-12-03 12:13:48 +0000

    Changes to be committed:

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from utils.tipoDados import tipoDados
 from utils.funcoesAnonimas import funcoesAnonimas, funcoesAnonimas2
 from exs.ex1 import soma
 from exs.ex2 import obterTemperatura
-from exs.ex5 import processar_nomes
-from exs.ex4 import acumulador_media,executar_acumulador_media
+from exs.ex4 import executar_acumulador_media
+
 
 from exs.ex5 import processar_nomes
 from exs.ex3 import maiusculas_palavras
@@ -12,7 +12,7 @@ def main():
     funcoesAnonimas()
     funcoesAnonimas2()
     soma()
-    acumulador_media(executar_acumulador_media())
+    executar_acumulador_media()
     obterTemperatura()
     processar_nomes()
     maiusculas_palavras()
