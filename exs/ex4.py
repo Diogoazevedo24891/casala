947eb8891047f71394ef7b9a05fcdfd4b49326ac
Diogoazevedo24891 <a24891@alunos.ipca.pt>
2025-12-03 12:12:24 +0000

    modified:   exs/ex4.py 	modified:   main.py

--- exs/ex4.py
+++ exs/ex4.py
@@ -2,18 +2,15 @@ from functools import reduce
 
 valores = [3.45, 2.78, 4.91, 5.03]
 
-# Função para acumular a soma e a contagem.
+# Função usada pelo reduce
 def acumulador_media(acc, val):
     soma_atual, contagem_atual = acc
     return (soma_atual + val, contagem_atual + 1)
 
-# Uso do reduce para obter soma e número de elementos
-soma, n = reduce(acumulador_media, valores, (0, 0))
+def executar_acumulador_media():
+    soma, n = reduce(acumulador_media, valores, (0, 0))
+    media = soma / n
 
-# Cálculo da média
-media = soma / n
-
-# Impressão do resultado formatado
-print(f"Média: {media:.2f}")
-print(f"Soma total: {soma}")
-print(f"Contagem (n): {n}")
+    print(f"Média: {media:.2f}")
+    print(f"Soma total: {soma}")
+    print(f"Contagem (n): {n}")
